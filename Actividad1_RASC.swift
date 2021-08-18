75739a7d779c53088dbf0c6df558530657a78989
AndreSC5008 <89164645+AndreSC5008@users.noreply.github.com>
2021-08-18 16:10:35 -0500

    Add files via upload

---
+++ Actividad1_RASC.swift
@@ -0,0 +1,14 @@
+/*Autor: Roberto André Sánchez Curiel
+Fecha: 18-08-2021
+Descripcción: Actividad 1- Lenguaje de programación moderno. 
+*/
+import Foundation
+
+var v1 = "esto es Swift"
+let c1 = 4
+var cadena:String = String()
+
+cadena = "Hola mundo " + v1 
+
+print("El contenido es: \(cadena)")
+print("El valor de constante es: \(c1)")
