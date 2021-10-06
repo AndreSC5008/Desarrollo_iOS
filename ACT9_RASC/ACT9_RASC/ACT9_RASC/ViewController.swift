069cef899db86c53a165307ae277f94b069e27e1
AndreSC5008 <89164645+AndreSC5008@users.noreply.github.com>
2021-10-06 15:15:07 -0500

    Add files via upload

---
+++ ACT9_RASC/ACT9_RASC/ACT9_RASC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ACT9_RASC
+//
+//  Created by user188672 on 10/6/21.
+//  Copyright © 2021 user188672. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ACT9_RASC/ACT9_RASC/ACT9_RASCTests/ACT9_RASCTests.swift
@@ -0,0 +1,34 @@
+//
+//  ACT9_RASCTests.swift
+//  ACT9_RASCTests
+//
+//  Created by user188672 on 10/6/21.
+//  Copyright © 2021 user188672. All rights reserved.
+//
+
+import XCTest
+@testable import ACT9_RASC
+
+class ACT9_RASCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
