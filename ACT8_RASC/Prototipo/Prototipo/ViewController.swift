268bd213920a2cf5fb4dc6c6c7eaaeedad467f20
AndreSC5008 <89164645+AndreSC5008@users.noreply.github.com>
2021-10-06 15:01:47 -0500

    Add files via upload

---
+++ ACT8_RASC/Prototipo/Prototipo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototipo
+//
+//  Created by user188672 on 10/5/21.
+//  Copyright © 2021 user188672. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
