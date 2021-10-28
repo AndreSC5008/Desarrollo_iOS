ed587a7d246427332990a4b63e66110d1e0ee615
AndreSC5008 <89164645+AndreSC5008@users.noreply.github.com>
2021-10-27 22:23:10 -0500

    Add files via upload

---
+++ GF14/GF14/GF14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GF14
+//
+//  Created by user188672 on 10/26/21.
+//  Copyright © 2021 user188672. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GF14/GF14/GF14Tests/GF14Tests.swift
@@ -0,0 +1,34 @@
+//
+//  GF14Tests.swift
+//  GF14Tests
+//
+//  Created by user188672 on 10/26/21.
+//  Copyright © 2021 user188672. All rights reserved.
+//
+
+import XCTest
+@testable import GF14
+
+class GF14Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
